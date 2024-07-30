209d2e386a2be7cd9e6373765263f40aed543318
weraseemann <wera.seemann@gmail.com>
2024-07-30 12:46:45 +0200

    change to getEvents

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,7 @@
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable testing-library/no-container */
+/* eslint-disable testing-library/no-node-access */
 // src/__tests__ / App.test.js
 
 import React from 'react';
@@ -5,7 +9,12 @@ import { render } from '@testing-library/react';
 import App from '../App';
 import '@testing-library/jest-dom/extend-expect';
 import { getEvents } from '../api'; // Mock the getEvents functionimport { getEvents } from '../api'; // Mock the getEvents function
-jest.mock('../api');
+
+jest.mock('../api', () => ({
+    getEvents: jest.fn(),
+}));
+
+getEvents.mockResolvedValue([]);
 
 describe('<App /> component', () => {
     let AppDOM;
