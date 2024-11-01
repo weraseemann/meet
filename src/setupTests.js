71a6b9a2a702b6a0f6f89c51eae51241fea74489
weraseemann <wera.seemann@gmail.com>
2024-11-01 18:01:55 +0100

    Fixing brocken test

--- src/setupTests.js
+++ src/setupTests.js
@@ -21,4 +21,21 @@ console.error = (...args) => {
     if (!ignoreMessage) originalError(...args);
 }
 
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
+
 jest.setTimeout(60000);
